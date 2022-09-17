d105dab2775d21a540410184ff92f93ca072370d
juliarose <julialunarose@gmail.com>
2022-09-17 00:16:57 -0400

    Initial Commit

---
+++ Beach/BeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeachApp.swift
+//  Beach
+//
+//  Created by colors on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct BeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
